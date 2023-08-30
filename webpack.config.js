57c1821971c33c79a970117502bddd1d94b8fa98
温海靖 <wenhaijing@hymson.com>
2023-08-30 15:41:26 +0800

    feat:启动devServer以及引入CSS处理兼容性

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
+console.log("process.env.NODE_ENV=", process.env.NODE_ENV);
+
+const config = {
   mode: "development", // 模式
   entry: "./src/index.js", // 打包入口文件
   output: {
@@ -14,7 +16,7 @@ module.exports = {
       //转换规则
       {
         test: /\.css$/, // 匹配所有css文件
-        use: "css-loader", // use:对应的loader名称
+        use: ["style-loader", "css-loader", "postcss-loader"], // use:对应的loader名称
       },
     ],
   },
@@ -24,4 +26,14 @@ module.exports = {
     }),
     new CleanWebpackPlugin(), // 引入目录清空插件
   ],
+  devServer: {
+    static: path.resolve(__dirname, "public"), // 静态文件目录
+    compress: true, // 是否开启gzip压缩
+    port: 8080, // 端口号
+  },
+};
+
+module.exports = (env, argv) => {
+  console.log("argv.mode=", argv.mode); // 打印mode
+  return config;
 };
