93fe9f4072c0f0d353132fc34b8897daf7fce0ec
温海靖 <wenhaijing@hymson.com>
2023-08-31 14:22:13 +0800

    feat:通过MiniCssExtractPlugin插件分离样式文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 console.log("process.env.NODE_ENV=", process.env.NODE_ENV);
 
@@ -16,7 +17,12 @@ const config = {
       //转换规则
       {
         test: /\.(s[ac]|c)ss$/i, // 匹配所有sass/scss/css文件
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"], // use:对应的loader名称
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ], // use:对应的loader名称
       },
     ],
   },
@@ -25,6 +31,10 @@ const config = {
       template: "./src/index.html",
     }),
     new CleanWebpackPlugin(), // 引入目录清空插件
+    new MiniCssExtractPlugin({
+      // 引入css插件
+      filename: "[name].[hash:8].css",
+    }),
   ],
   devServer: {
     static: path.resolve(__dirname, "public"), // 静态文件目录
